ad1488ec4424e9f72bbbca31b5b0b618723c4d98
cristian maizon <ismael.maizon1234@gmail.com>
2023-06-04 23:16:40 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
+import viewsRouter from './routers/views.router.js'
+
+
+import { Server } from 'socket.io';
+
+
+const app = express();
+const httpServer = app.listen(8080, () => console.log('corriendo'));
+
+
+const io = new Server(httpServer);
+
+app.engine( "handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+
+app.use(express.static(__dirname+'/public'));
+app.use('/', viewsRouter)
+
+
+io.on('connection', (socket) => {
+    console.log('conectado');
+})
---
+++ src/utils.js
@@ -0,0 +1,10 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+
+
+const __filname = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filname);
+
+
+export default __dirname;
