5f11c26b8a42cc2b070dedcc5473253bc37020a8
cristian maizon <ismael.maizon1234@gmail.com>
2023-06-05 23:00:51 -0300

    segundo commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+// lado del backend
+
+
 import express from 'express';
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
@@ -22,6 +25,29 @@ app.use(express.static(__dirname+'/public'));
 app.use('/', viewsRouter)
 
 
+const mensajes = []
+
 io.on('connection', (socket) => {
-    console.log('conectado');
+    console.log('conectado: ' + socket.id);
+
+    socket.on('message', (data) => {
+        console.log(data);
+        mensajes.push(data);
+
+        console.log(mensajes);
+        io.emit( 'imprimir', mensajes );
+
+    })
+
+    // ingreso nuevo usuario
+    socket.on('authenticatedUser', (data) => { 
+        console.log('se conecto:' + data);
+        socket.broadcast.emit( 'ingreso', data);
+
+    })
+
 })
+
+// socket.emit('evento_para_socket_individual', 'este mensaje solo lo debe recibir el socket')
+// socket.broadcast.emit('evento_para_todos_menos_el_socket_actual', 'este evento lo verean los socket conectasdos, MENOS el socket actual desde donde se envio el mensaje')
+// socketServer.emit('evento_para_todos', 'este evento lo verean todos los socket conectasdos)
